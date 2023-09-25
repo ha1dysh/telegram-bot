0e82236078358d562aee9e17835900690ecdf9d6
ha1dysh <olekshaid@gmail.com>
2023-09-26 00:57:23 +0300

    fix max tokens

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -13,7 +13,7 @@ export class GptService {
     const res = await this.openai.chat.completions.create({
       model: 'gpt-3.5-turbo',
       messages: [{ role: 'user', content: message }],
-      max_tokens: 100,
+      max_tokens: 2000,
     });
 
     return res.choices[0].message.content;
